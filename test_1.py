0b32263af12752b9d3f2b84b20f17a458575941f
RohitDhankar <dhankar.rohit@gmail.com>
2022-02-05 21:50:13 +0530

    REQMTS_UPDATED >> torch_1_reqmts.txt

--- test_1.py
+++ test_1.py
@@ -3,3 +3,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+
+## https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+## Every TorchVision Dataset includes two arguments: transform and target_transform to modify the samples and labels respectively.
+
