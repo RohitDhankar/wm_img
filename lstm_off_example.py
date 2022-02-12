3d5d1f58826bd6fc013f98d92c7e7aaea6dc6dc2
RohitDhankar <dhankar.rohit@gmail.com>
2022-02-12 23:55:35 +0530

    All_Test_Loss---GARBAGE --->> test loss: 0.4794289653762204

--- lstm_off_example.py
+++ lstm_off_example.py
@@ -44,12 +44,24 @@ if __name__ == '__main__':
     np.random.seed(0)
     torch.manual_seed(0)
     # load data and make training set
-    data = torch.load('traindata.pt')
-    
+    #data = torch.load('traindata.pt')
+    data = torch.load('own_data_T_20_L_100_N_2.pt')
+
+    print("----initial_data___\n",data)
+
+
     input = torch.from_numpy(data[3:, :-1])
+    print("---input.shape----\n",input.shape)
     target = torch.from_numpy(data[3:, 1:])
+    print("---target.shape----\n",target.shape)
+
     test_input = torch.from_numpy(data[:3, :-1])
+    print("---test_input.shape----\n",test_input.shape)
     test_target = torch.from_numpy(data[:3, 1:])
+    print("---test_target.shape----\n",test_target.shape)
+    
+
+
     # build the model
     seq = Sequence()
     seq.double()
@@ -69,23 +81,26 @@ if __name__ == '__main__':
         optimizer.step(closure)
         # begin to predict, no need to track gradient here
         with torch.no_grad():
-            future = 1000
+            #future = 1000 ## Original 
+            future = 10 ## FOOBAR CHANGED 
             pred = seq(test_input, future=future)
             loss = criterion(pred[:, :-future], test_target)
             print('test loss:', loss.item())
             y = pred.detach().numpy()
+            print("-------PRED__y_pred.detach().numpy()__",y)
+
         # draw the result
         plt.figure(figsize=(30,10))
-        plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
-        plt.xlabel('x', fontsize=20)
-        plt.ylabel('y', fontsize=20)
-        plt.xticks(fontsize=20)
-        plt.yticks(fontsize=20)
+        plt.title(f'PRED_TEST_Loss __>>{loss.item()}', fontsize=10)
+        plt.xlabel('x', fontsize=10)
+        plt.ylabel('y', fontsize=10)
+        plt.xticks(fontsize=10)
+        plt.yticks(fontsize=10)
         def draw(yi, color):
             plt.plot(np.arange(input.size(1)), yi[:input.size(1)], color, linewidth = 2.0)
             plt.plot(np.arange(input.size(1), input.size(1) + future), yi[input.size(1):], color + ':', linewidth = 2.0)
         draw(y[0], 'r')
         draw(y[1], 'g')
-        draw(y[2], 'b')
-        plt.savefig('predict%d.pdf'%i)
+        #draw(y[2], 'b') ##IndexError: index 2 is out of bounds for axis 0 with size 2
+        plt.savefig('pred_official_%d_.pdf'%i)
         plt.close()
--- lstm_off_getData.py
+++ lstm_off_getData.py
@@ -4,14 +4,32 @@ import torch
 np.random.seed(2)
 
 T = 20
-L = 1000
-N = 100
+L = 100 # 1000
+N = 2 # 100
 
-x = np.empty((N, L), 'int64')
+#T_20_L_100_N_2
+x = np.empty((N, L), 'int64') #
+# Empty NumpyArray of shape --> N,L == Number of Arrays , Count of Elements in Each Array 
+
+print("---x.shape----\n",x.shape)
+print("---x.ndim----\n",x.ndim) # 2 - 2D_ARRAY with SHAPE ---> N,L ->> 2,10
+print("---x[:5]----\n",x[:5])
+print("  "*60)
+print("---x[:1]----\n",x[:1])
+print("  "*60)
+print("---x[:2]----\n",x[:2])
+print("  "*60)
+print("---x[:-10]----\n",x[:-10])
+
+
 x[:] = np.array(range(L)) + np.random.randint(-4 * T, 4 * T, N).reshape(N, 1)
 
 data = np.sin(x / 1.0 / T).astype('float64')
+
+print("---data.shape----\n",data.shape)
+print("---data[:5]----\n",data[:5])
+print("---data[:-10]----\n",data[:-10])
 print("------type(data)----",type(data)) ##- <class 'numpy.ndarray'>
 
-torch.save(data, open('traindata.pt', 'wb'))
+torch.save(data, open('own_data_T_20_L_100_N_2.pt', 'wb'))
 ## traindata.pt -- 1.3MB -- ZIP File 
